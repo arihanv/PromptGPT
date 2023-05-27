770bec487dbb7e46c9064b40a77636381e2f83c2
arihanv <arihanvaranasi@gmail.com>
2023-05-26 20:20:57 -0500

    Initial Files

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,47 @@
 import Link from "next/link"
+import { ArrowRight } from "lucide-react"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
+import { MiniBar } from "@/components/mini-bar"
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
+      <div className="flex flex-col items-center gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          PromptGPT
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+        <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl text-center">
+          Instantly generate an <b>efficient</b> prompt for an LLM{" "}
+          <br className="hidden sm:inline" /> with just a <b>few</b> clicks.
         </p>
       </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants({ size: "lg" })}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline", size: "lg" })}
-        >
-          GitHub
-        </Link>
+      <div className="flex flex-col gap-4 items-center">
+        <div className="bg-gray-200 dark:bg-gray-800 p-1.5 rounded-lg w-fit">
+          <MiniBar />
+        </div>
+        <div className="flex gap-4 justify-center">
+          <div className="bg-white dark:bg-black min-w-[415px] max-h-[500px] min-h-[400px] rounded-xl p-1 border border-gray-700 shadow-lg">
+            <div className="h-full w-full rounded-lg border border-gray-700 bg-white dark:bg-black overflow-y-scroll">
+              <Textarea
+                className="h-full p-2.5"
+                placeholder="Type your prompt here."
+              />
+            </div>
+          </div>
+          <div className="h-full flex my-auto">
+            <ArrowRight />
+          </div>
+          <div className="bg-white dark:bg-black min-w-[415px] max-h-[500px] min-h-[400px] rounded-xl p-1 border border-gray-700 shadow-lg">
+            <div className="h-full w-full rounded-lg border border-gray-700 bg-white dark:bg-black overflow-y-scroll">
+              <div className="p-2.5 opacity-50 text-sm italic font-medium">
+                Your prompt will appear here...
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "PromptGPT",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
@@ -9,10 +9,14 @@ export const siteConfig = {
       title: "Home",
       href: "/",
     },
+    {
+      title: "Statistics",
+      href: "/statistics",
+    },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "/",
+    github: "/",
+    docs: "/",
   },
 }
