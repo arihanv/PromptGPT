2a46ec08af26f605c7bdbd5aabf84a1d505327f9
arihanv <arihanvaranasi@gmail.com>
2023-05-30 18:53:13 -0500

    Implemented API Fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
+"use client"
+
+import { useState } from "react"
 import Link from "next/link"
-import { ArrowDown, ArrowRight } from "lucide-react"
+import { ArrowDown, ArrowRight, Loader2 } from "lucide-react"
 
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 import { Textarea } from "@/components/ui/textarea"
 import { MiniBar } from "@/components/mini-bar"
 
 export default function IndexPage() {
+  const [input, setInput] = useState("")
+  const [output, setOutput] = useState("")
+  const [loading, setLoading] = useState(true)
+
+  async function getQuery(query: string) {
+    const res = await fetch(
+      `https://gpt-api-z4khdeirua-uc.a.run.app/predict/${query}`
+    )
+
+    console.log(
+      "Asked:",
+      `https://gpt-api-z4khdeirua-uc.a.run.app/predict/${query}`
+    )
+
+    const data = await res.json()
+    console.log(data[0].revised)
+    setOutput(data[0].revised)
+    setLoading(false)
+  }
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 w-fit">
       <div className="flex flex-col items-center gap-2">
@@ -23,12 +46,24 @@ export default function IndexPage() {
           <MiniBar />
         </div>
         <div className="lg:flex-row flex-col flex gap-4 justify-center items-center flex-wrap lg:items-stretch">
-          <div className="bg-white dark:bg-black min-w-[315px] lg:min-w-[400px] max-h-[500px] lg:min-h-[410px] min-h-[200px] rounded-xl p-1 border border-gray-700 shadow-lg grid">
-            <div className="flex w-full rounded-lg border border-gray-700 bg-white dark:bg-black textareaCont">
+          <div className="bg-white relative dark:bg-black min-w-[315px] lg:min-w-[400px] max-h-[500px] lg:min-h-[410px] min-h-[200px] rounded-xl p-1 border border-gray-700 shadow-lg grid">
+            <div className="flex w-full rounded-lg border border-gray-700 bg-white dark:bg-black h-full flex-col">
               <Textarea
-                className="h-full p-2.5 resize-none"
+                onChange={(e) => setInput(e.target.value)}
+                className="h-full p-2.5 resize-none rounded-b-none"
                 placeholder="Type your prompt here."
               />
+              <Button
+                onClick={(e) => {
+                  e.preventDefault()
+                  setLoading(true)
+                  getQuery(input)
+                }}
+                className="rounded-t-none"
+                variant="default"
+              >
+                Send Query
+              </Button>
             </div>
           </div>
           <div className="h-full flex my-auto">
@@ -39,11 +74,27 @@ export default function IndexPage() {
               <ArrowDown />
             </div>
           </div>
-          <div className="bg-white dark:bg-black min-w-[315px] lg:min-w-[400px] max-h-[500px] lg:min-h-[410px] min-h-[200px] rounded-xl p-1 border border-gray-700 shadow-lg grid">
+          <div className="bg-white dark:bg-black w-[315px] lg:w-[400px] max-h-[500px] lg:h-[410px] h-[200px] rounded-xl p-1 border border-gray-700 shadow-lg grid">
             <div className="flex w-full rounded-lg border border-gray-700 bg-white dark:bg-black overflow-y-scroll">
-              <div className="p-2.5 opacity-50 text-sm italic font-medium">
-                Generated prompt will appear here...
-              </div>
+              {!output ? (
+                <div className="p-2.5 opacity-50 text-sm italic font-medium">
+                  Generated prompt will appear here...
+                </div>
+              ) : (
+                <>
+                  {!loading ? (
+                    <div className="p-2.5 opacity-50 text-sm font-medium">
+                      {output}
+                    </div>
+                  ) : (
+                    <div className="flex w-full">
+                      <div className="animate-spin text-gray-400 repeat-infinite dark:text-gray-600 m-auto">
+                        <Loader2 size={30} />
+                      </div>
+                    </div>
+                  )}
+                </>
+              )}
             </div>
           </div>
         </div>
