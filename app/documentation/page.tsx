563c5a045057fbd802550f3b7c7214bd23f140bc
John Hua <117111046+johnhuatech@users.noreply.github.com>
2023-06-03 16:02:53 -0700

    Adding Two Pages Into Nav Bar.
    Adding two new folders to start two new pages within the navigation bar for team page & documentation.

---
+++ app/documentation/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import { ArrowRight } from "lucide-react"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/button"
+import { Textarea } from "@/components/ui/textarea"
+
+export default function StatsPage() {
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+          Statistics
+        </h1>
+        <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl text-center">
+          View the <b>amazing</b> statics <br className="hidden sm:inline" /> of
+          our prompt generation tool.
+        </p>
+      </div>
+    </section>
+  )
+}
